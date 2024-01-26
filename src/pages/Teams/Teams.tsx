732242fde8b6c6d61c0ddea5e2bb35e906be5e5c
ConanKudo <ahmedhtml.art@gmail.com>
2024-01-26 22:23:10 +0800

    Batman

--- src/pages/Teams/Teams.tsx
+++ src/pages/Teams/Teams.tsx
@@ -4,7 +4,7 @@ const Teams: React.FC = () => {
   return (
     <div>
       <h1>This is the Teams listing page</h1>
-      {/* Add your component code here */}
+      {/* BATMAN */}
     </div>
   );
 };
